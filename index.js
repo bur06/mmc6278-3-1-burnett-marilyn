e5ae12240fb4de7c9ac74a0912d84cb624779b42
mgburnett <marilyn.burnett442@comcast.net>
2022-05-27 11:17:50 -0500

    array syntax

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 function runQuiz() {
 var questionsArr = [
     {
-      question: 'Gatorade formulated at UF',
+      question: 'Gatorade was formulated at UF',
       answer: true
-    }
+    },
     {
-      question: 'There is only one mascot.'
+      question: 'There is only one mascot.',
       answer: false
-    }
+    },
     {
-      question: 'UF has had three Heisman winning quarterbacks.'
+      question: 'UF has had three Heisman winning quarterbacks.',
       answer: true
-    }
+    },
     {
-      question: 'UF football field is nicknamed The Swamp.'
+      question: 'UF football field is nicknamed The Swamp.',
       answer: true
-    }
+    },
     {
-      question: 'Tim Tebow graduated UF after playing in the NFL.'
+      question: 'Tim Tebow graduated UF after playing in the NFL.',
       answer: false
     }
 ]
