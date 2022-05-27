fc6205fc7ca61d1a0d72e2851571a416657bc302
mgburnett <marilyn.burnett442@comcast.net>
2022-05-27 13:00:23 -0500

    if else stmt

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 // Your code here
 function runQuiz() {
-var questionsArr = [
-    {
-      question: 'Gatorade was formulated at UF',
-      answer: true
-    },
-    {
-      question: 'There is only one mascot.',
-      answer: false
-    },
-    {
-      question: 'UF has had three Heisman winning quarterbacks.',
-      answer: true
-    },
-    {
-      question: 'UF football field is nicknamed The Swamp.',
-      answer: true
-    },
-    {
-      question: 'Tim Tebow graduated UF after playing in the NFL.',
-      answer: false
+    var questionsArr = [
+        {
+        question: 'Gatorade was formulated at UF',
+        answer: true
+        },
+        {
+        question: 'There are two UF mascots.',
+        answer: true
+        },
+        {
+        question: 'UF has had three Heisman winning quarterbacks.',
+        answer: true
+        },
+        {
+        question: 'UF football field is nicknamed The Swamp.',
+        answer: true
+        },
+        {
+        question: 'Tim Tebow graduated UF before playing in the NFL.',
+        answer: true
+        }
+    ]
+
+    var numOfTrues = 0;
+    for (var i=0; i<questionsArr.length; i++) {
+        var ask = questionsArr[i];
+        var answers = confirm(ask);
+        if (answers) {
+            numOfTrues++;
+        }
     }
-]
+    if (numOfTrues = 5) {
+        alert("Congrats! You gave all correct answers.");
+    } else {
+        alert("Sorry, one or more answers are not correct.");
+    }
 
-for (var i=0; i<questionsArr.length; i++) {
-    var ask = questionsArr[i];
-    var answers = confirm(ask)
 }
 
-}
 //runQuiz()
