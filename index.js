734ed2c286c143d13012c36fa3de5facc4b826fa
mgburnett <marilyn.burnett442@comcast.net>
2022-05-27 11:09:32 -0500

    question arrary

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function runQuiz() {
+var questionsArr = [
+    {
+      question: 'Gatorade formulated at UF',
+      answer: true
+    }
+    {
+      question: 'There is only one mascot.'
+      answer: false
+    }
+    {
+      question: 'UF has had three Heisman winning quarterbacks.'
+      answer: true
+    }
+    {
+      question: 'UF football field is nicknamed The Swamp.'
+      answer: true
+    }
+    {
+      question: 'Tim Tebow graduated UF after playing in the NFL.'
+      answer: false
+    }
+]
+
+}
